7347eaaa23d9067dc143b25c7131a054bc87dc35
Josue Castro <josue06castp@gmail.com>
2021-07-22 20:47:01 -0500

    Union Repo Anterior

--- presentacion/inicio.php
+++ presentacion/inicio.php
@@ -1,13 +1,11 @@
 
 <div id="LogoWL-container">
-    <a href="/">
-        <img id= "LogoWL" src="img/LogoWL.svg" alt="LogoWL">
-    </a>
+    <img id= "LogoWL" src="img/LogoWL.svg" alt="LogoWL">
 </div>
 
-    <div class="botones_tabla-container" style="margin: 5% 20%; display: flex;">
+    <div class="botones_tabla-container">
 
-        <div class="botones-container" style="display: block; align-self: center;">
+        <div class="botones-container">
             <div>
                 <a class="btn btn__1" href="index.php?pid=<?php echo base64_encode("presentacion/lista/CrearLista.php")?>" style="text-decoration: none;">Crear Lista</a>
             </div>
@@ -16,20 +14,20 @@
             </div>
         </div>
 
-        <div class="table-container" style="display: block;">
+        <div class="table-container">
 
             <table class="table">
 
                 <tr>
-                    <td class="table-title">Canción 1 <br><p style="font-size: 15px; margin: 0;">autor</p></td>
+                    <td class="table-title">Canción 1<br><p style="font-size: 15px; margin: 0;">autor</p></td>
                     <td class="ir-cancion"><a href="#"><img src="img/next.svg" style="width: 20px;"></a></td>
                 </tr>
                 <tr>
-                    <td class="table-title">Canción 2</td>
+                    <td class="table-title">Canción 2<br><p style="font-size: 15px; margin: 0;">autor</p></td>
                     <td class="ir-cancion"><a href="#"><img src="img/next.svg" style="width: 20px;"></a></td>
                 </tr>
                 <tr>
-                    <td class="table-title">Canción 3</td>
+                    <td class="table-title">Canción 3<br><p style="font-size: 15px; margin: 0;">autor</p></td>
                     <td class="ir-cancion"><a href="#" ><img src="img/next.svg" style="width: 20px;"></img></a></td>
                 </tr>
 
