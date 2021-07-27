1f59421c79ce263c4d5e4b4e1b24854c3834a17e
Daniel Callejas <dcallejas@unicolmayor.edu.co>
2021-07-27 11:14:57 -0500

    Estructura: crear canción.

--- presentacion/cancion/CrearCancion.php
+++ presentacion/cancion/CrearCancion.php
@@ -5,4 +5,13 @@
     <input type="text" id="name" class="nombre-input" placeholder="Nombre de la canción">
     <input type="autor" id="autor" class="autor-input" placeholder="Nombre de la canción">
 
+    <datalist class="datalist-tipo">
+
+        <option value="Alabanza">
+        <option value="Adoración">
+
+    </datalist>
+
+    <input type="submit">
+
 </form>
